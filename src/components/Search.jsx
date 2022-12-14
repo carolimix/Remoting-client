ba1a0a9e7922d7667391e1add0bf6e056cd1efa9
Carolina Acuna <carolina.acuna90@gmail.com>
2022-12-14 01:41:04 +0100

    search function, details

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Search() {
+    const [search, setSearch] = useState("");
+    const [ data, setData] = useState("");
+
+    useEffect(() => {
+        axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/spaces`)
+      .then((response) => setData(response.data));
+  }, []);
+
+    const searched = data.filter((result) => {
+        if (!result.name) {
+            return false;
+        } else if (!result.district) {
+            return true;
+        } else {
+            return (
+                result.name.toLowerCase().includes(search.toLowerCase) ||
+                result.district.toLowerCase().includes(search.toLowerCase)
+
+            )
+        }
+
+    })
+
     return (
-        <div>
+              <div className=" bg-gray-300 ">
             <h1>Search</h1>
-        </div>
+            <input
+                placeholder="Search"
+                type="text"
+                value={search}
+                onChange={(e) => {
+                    setSearch(e.target.value);
+                }}
+                className=" w-96 border rounded border-gray-400 h-10 focus:outline-none pl-4 pr-8 text-gray-700 text-sm text-gray-500"
+            />
+            </div>
     );
 }
 
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -22,23 +22,26 @@ function Details() {
   }, []);
 
   return (
-    <div className="spacesDetails">
-           {space && 
-      <div className="card"
-      style={{ width: "20rem", height: "30rem" }}>
-        <img className="img-card" src={space.imageUrl} alt="Image of the place" /> 
-        <h2>{space.name} </h2>
-        <h3>{space.district}</h3>
-        <p>{space.description}</p>
-        <p>{space.type}</p>
-        <p>{space.priceRange}</p>
-        <p>{space.openingTimes}</p>
-        <p>{space.website}</p>
-        <p>{space.petFriendly}</p>
-        <p>{space.extras}</p>
-           </div>      
-      }      
-{/*       <Link to="/spaces">Back to all the options</Link> */}
+    <div className="bg-red-100 items-center container w-screen px-2 md:px-12 my-6 mx-auto">
+      <div className="flex item-stretch -mx-1 lg:-mx-2">
+        {space && (
+          <div className="my-1 mx-1 px-1 w-full md: w-1/3 lg:my-4 lg:px-4 lg:w-1/5">
+            <img src={space.imageUrl} alt="Image of the place" />
+            <h2>-{space.name}-</h2>
+            <h3>📍{space.district}</h3>
+            <p>{space.description}</p>
+            <p>☕{space.type}</p>
+           {/*  <p>{space.priceRange}</p> */}
+            <a href={space.website} target="_blank">{space.website}</a>
+            <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{space.type}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{space.district}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#petFriendly</span>
+      </div>
+            <p><i>"{space.extras}"</i></p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
